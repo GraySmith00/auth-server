a043aa30297452a82a5eb0dfb55f77c0c5f4ad94
Gray Smith <gray.smith00@gmail.com>
2018-11-26 21:05:09 -0700

    Build out basic server file (index.js)

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const router = require('./router');
+require('dotenv').load();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// DB Setup
+const db = process.env.MONGO_URI;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
