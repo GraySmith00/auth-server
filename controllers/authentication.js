9c141a90f4f41f50670c078ce84b193d00800015
Gray Smith <gray.smith00@gmail.com>
2018-11-26 21:05:41 -0700

    Set up authentication controller

---
+++ controllers/authentication.js
@@ -0,0 +1,11 @@
+exports.signup = (req, res, next) => {
+  // see if user with email exists
+  console.log(req.body);
+
+  // if user with email exists return an error
+
+  // if email does not exist, create and save
+
+  // respond to request
+  res.send({ success: true });
+};
