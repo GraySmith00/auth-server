b2c4f341ef66d65669e803b2b8c484f8a0199fd8
Gray Smith <gray.smith00@gmail.com>
2018-11-26 21:54:59 -0700

    Install bcrypt-nodejs and add password salt pre save to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define model
 const userSchema = new Schema({
@@ -9,6 +10,19 @@ const userSchema = new Schema({
     lowercase: true
   },
   password: String
+});
+
+// Encrypt password on save
+userSchema.pre('save', async function(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create model class
