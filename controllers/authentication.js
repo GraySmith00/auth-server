533a9bb1fdb4898dc37270d993a97e306ab1a524
Gray Smith <gray.smith00@gmail.com>
2018-11-26 21:54:31 -0700

    Build out authentication signup action

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,11 +1,30 @@
-exports.signup = (req, res, next) => {
-  // see if user with email exists
-  console.log(req.body);
+const User = require('../models/user');
 
-  // if user with email exists return an error
+exports.signup = async (req, res, next) => {
+  const { email, password } = req.body;
+  const requiredParams = ['email', 'password'];
 
-  // if email does not exist, create and save
+  for (let param of requiredParams) {
+    if (!req.body[param]) {
+      return res
+        .status(422)
+        .json({
+          error: `Expected format: { email: <String>, password: <String> }. You're missing a "${param}" property.`
+        });
+    }
+  }
 
-  // respond to request
-  res.send({ success: true });
+  try {
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(422)
+        .json({ error: 'A user with that email already exists.' });
+    }
+    const user = new User({ email, password });
+    await user.save();
+    res.json({ _id: user._id, email: user.email });
+  } catch (err) {
+    return next(err);
+  }
 };
